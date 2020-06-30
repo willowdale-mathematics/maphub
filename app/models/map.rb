2a9390a03fbae726ec417da585dc313ca64b82ac
Jeremy Udit <jcudit@github.com>
2020-06-30 16:58:17 -0400

    Add Support For Distance / Shortest Path

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,5 +1,24 @@
+require 'dijkstra_graph'
+
 class Map < ApplicationRecord
-  def distance(path)
-    return 4.0
+
+  def graph
+    if @graph.nil?
+     @graph = DijkstraGraph::Graph.new
+      paths["buildings"].each do |p, building|
+        building.each do |k, edge|
+          @graph.add_edge(p, k, edge)
+        end
+      end
+    end
+    @graph
+  end
+
+  def shortest_path(source, destination)
+    graph.shortest_path(source, destination)
+  end
+
+  def distance(source, destination)
+    graph.shortest_distances(source)[destination]
   end
 end
--- test/models/map_test.rb
+++ test/models/map_test.rb
@@ -6,7 +6,16 @@ class MapTest < ActiveSupport::TestCase
   end
 
   test "should calclulate a distance given a path" do
-    paths = ["a", "b", "c"]
-    assert_equal @map.distance(paths), 4.0
+    source = "a"
+    destination = "c"
+    expected_distance = 4.0
+    assert_equal expected_distance, @map.distance(source, destination)
+  end
+
+  test "should shortest path" do
+    source = "a"
+    destination = "c"
+    expected_path = ["a", "b", "c"]
+    assert_equal expected_path, @map.shortest_path(source, destination)
   end
 end
